9ca9752ed919ebdf786db947c9c1a3e774de3a28
Sergio Pérez <sergiom.perezprieto@gmail.com>
2021-04-20 17:08:45 +0200

    Delete src/Practice1 directory

--- src/Practice1/calc_Es.m
+++
@@ -1,9 +0,0 @@
-function Es = calc_Es(ordenConstelacion, modulacion)
-    if strcmp(modulacion,'PAM')
-        Es = ((ordenConstelacion.^2)-1)/3;
-    elseif strcmp(modulacion,'QAM')
-        Es = (2*(ordenConstelacion - 1))/3;
-    else
-        error('La modulación no es ni M-PAM ni M-QAM');
-    end
-end
