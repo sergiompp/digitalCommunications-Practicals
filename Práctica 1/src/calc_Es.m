4f46b7035c751e0bff1ca8062ebf75d2f9c58ce6
Sergio Pérez <sergiom.perezprieto@gmail.com>
2021-11-01 16:26:56 +0100

    Update calc_Es.m

--- Práctica 1/src/calc_Es.m
+++ Práctica 1/src/calc_Es.m
@@ -1,4 +1,14 @@
 function Es = calc_Es(ordenConstelacion, modulacion)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Función que determina la energia por simbolo (Es)
+% 
+% PARAMETROS ENTRADA:
+% ordenConstelacion: Valor de M de la modulacion (Ej. 2-PAM -> M=2)
+% modulacion: Si se emplea modulación de tipo 'PAM' o 'QAM' 
+%
+% PARAMETROS SALIDA:
+% Es: Energia (J)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if strcmp(modulacion,'PAM')
         Es = ((ordenConstelacion.^2)-1)/3;
     elseif strcmp(modulacion,'QAM')
